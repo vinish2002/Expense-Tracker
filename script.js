91074320c0236c151a4590d491013c9bf3a239c3
Vinish <sunkuvinish2002@gmail.com>
2025-01-02 19:27:30 +0530

    Commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const expenseForm = document.getElementById("expense-form");
 const expenseList = document.getElementById("expense-list");
 const totalAmountElement = document.getElementById("total-amount");
 
-// Initialize expenses array from localStorage
+// Initialize expenses array from localStorage--------------------------------------------
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 
 // Function to render expenses in tabular form
